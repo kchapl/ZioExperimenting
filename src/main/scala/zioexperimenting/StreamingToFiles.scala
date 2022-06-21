f3a4155872ac76ab45e10069aa1f6f68da9a98a6
kchapl <kchapl@users.noreply.github.com>
2022-06-21 21:38:04 +0100

    Reduce concurrent count

--- src/main/scala/zioexperimenting/StreamingToFiles.scala
+++ src/main/scala/zioexperimenting/StreamingToFiles.scala
@@ -4,13 +4,15 @@ import zio._
 import zio.stream.ZStream
 
 import java.nio.charset.StandardCharsets.UTF_8
-import java.nio.file.{Files, Paths}
 import java.nio.file.StandardOpenOption.CREATE_NEW
+import java.nio.file.{Files, Paths}
+import java.time.temporal.ChronoUnit.MILLIS
 
 object StreamingToFiles extends ZIOAppDefault {
 
   private val numbers = 1 to 2500000
   private val batchSize = 10000
+  private val concurrentCount = 7
 
   private def toBytes(n: Int) =
     String.valueOf(s"$n\n").getBytes(UTF_8)
@@ -34,15 +36,14 @@ object StreamingToFiles extends ZIOAppDefault {
     ZStream
       .fromIterable(numbers)
       .grouped(batchSize)
-      .mapZIOParUnordered(numbers.length / batchSize)(writeToFile)
+      .mapZIOParUnordered(concurrentCount)(writeToFile)
       .foreach(_ => ZIO.unit)
 
-  override def run: ZIO[ZIOAppArgs, Any, Any] =
+  override def run: ZIO[Any, Throwable, Unit] =
     for {
       startTime <- Clock.currentDateTime
-      _ <- Console.printLine(startTime)
       _ <- program
       endTime <- Clock.currentDateTime
-      _ <- Console.printLine(endTime)
+      _ <- Console.printLine(s"Took ${MILLIS.between(startTime, endTime)} ms")
     } yield ()
 }
